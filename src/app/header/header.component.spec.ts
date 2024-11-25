3ed43166b70ba26a27a7a727981a6b7ae6ddb59d
icarodredd <icarog854@gmail.com>
2024-11-25 14:08:38 -0300

    test: :white_check_mark: add header test

---
+++ src/app/header/header.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display a header with a text of "todo"', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const h1 = headerElement.querySelector('h1')!;
+    expect(h1.textContent).toEqual('todo');
+  });
+});
